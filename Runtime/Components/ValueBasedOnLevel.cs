a2f233453bf996cf77365cff5e414fb12d6c9cf2
ariatophanes <ariatophanes@gmail.com>
2021-11-24 20:59:54 +0300

    Тест

--- Runtime/Components/ValueBasedOnLevel.cs
+++ Runtime/Components/ValueBasedOnLevel.cs
@@ -39,9 +39,10 @@ namespace LittleBit.Modules.Description.Components
             if (!_growthFunction) return;
 
             List<Keyframe> keyframes = new List<Keyframe>();
+            
             for (int i = 1; i < _maxLevel; i++)
             {
-                var keyframe = new Keyframe(i-1, (float) GetValue(i-1));
+                var keyframe = new Keyframe(i, (float) GetValue(i-1));
 
                 _keyCurves.Add(new KeyCurve(keyframe));
                 keyframes.Add(keyframe);
